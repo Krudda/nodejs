f54bd71028527e2c33edc10f97cd62efe5b69978
Dmitriy <krudda@yandex.ru>
2023-03-23 18:47:45 +0300

    feat - use pipeline instead pipe

--- task-1/task1_2.js
+++ task-1/task1_2.js
@@ -2,27 +2,23 @@ import { createReadStream, createWriteStream } from "fs";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import csvtojson from 'csvtojson';
+import { pipeline } from "stream/promises";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const writeFilepath = __dirname + "/parsed.txt";
 const readFilePath = __dirname + '/csv/library.csv';
 
 const parseFile = async () => {
-	const readStream = createReadStream(readFilePath);
-	const writeStream = createWriteStream(writeFilepath);
-
-	readStream
-		.pipe(csvtojson())
-		.on('data', async data => {
-			console.log('data', data.toString());
-			writeStream.write(data);
-		})
-		.on('error', (err) => {
-			console.error('Failed to parse file')
-		})
-		.on('end', () => {
-			console.log('File successfully parsed')
-		});
+	try {
+		pipeline(
+			createReadStream(readFilePath), 
+			csvtojson({ delimiter: ';' }),
+			createWriteStream(writeFilepath)
+		)
+		console.log('File parsed');
+	} catch {
+		console.error('Failed to parse file')
+	}
 }
 
 await parseFile();
