453d0086c8de4139ca58e66e0489c69d0aa3e87e
Dmitriy <krudda@yandex.ru>
2023-05-01 20:55:12 +0300

    feat - add email validation

--- task-3/schemas/userSchema.js
+++ task-3/schemas/userSchema.js
@@ -13,22 +13,21 @@ const schema = {
         },
         password: {
             type: 'string',
-            // pattern: '^(?=.*[A-Z].*[A-Z])(?=.*[!@#$&*])(?=.*[0-9].*[0-9])(?=.*[a-z].*[a-z].*[a-z]).{8,}$',
             minLength: 8,
             maxLength: 20,
         },
         email: {
             type: 'string',
+            pattern: '/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu',
         },
-        // isDeleted: { type: 'boolean' },
     },
     required: ['username', 'password', 'email'],
     additionalProperties: true,
     errorMessage: {
         properties: {
-            username: 'Login must be a string longer than 2 characters',
-            password: 'The password must be a string of at least 8 characters, contain uppercase and lowercase letters, numbers and special characters.',
-            email: 'Invalid email',
+            username: 'User name must be a string longer than 2 characters',
+            password: 'The password must be a string of at least 8 characters.',
+            email: 'Invalid email.',
         },
     },
 };
