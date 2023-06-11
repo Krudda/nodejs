554e063a2b61b4aec36b9fded1fd0cac7a629706
Dmitriy <krudda@yandex.ru>
2023-06-12 00:35:44 +0300

    feat - add unhandledRejection handler

--- task-5/index.js
+++ task-5/index.js
@@ -13,6 +13,9 @@ process.on('uncaughtException', (error) => {
     console.error('Uncaught Exception:', error);
     process.exit(1);
 });
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
 
 server.use(express.json());
 server.use(logger);
